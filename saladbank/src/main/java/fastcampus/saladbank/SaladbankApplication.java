5e0a74c8c6acf2ff535b783fbe345f6584cbe252
JeongSu <zz1996zz@naver.com>
2022-08-24 00:51:25 +0900

    create: create saladbank project

---
+++ saladbank/src/main/java/fastcampus/saladbank/SaladbankApplication.java
@@ -0,0 +1,13 @@
+package fastcampus.saladbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaladbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaladbankApplication.class, args);
+	}
+
+}
---
+++ saladbank/src/test/java/fastcampus/saladbank/SaladbankApplicationTests.java
@@ -0,0 +1,13 @@
+package fastcampus.saladbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaladbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
