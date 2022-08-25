5c6cd8eba8eb452a54449c8cec4e50221a310f24
wijoonwu <devjoon@nate.com>
2022-08-26 01:28:10 +0900

    @RequiredArgsConstructor 어노테이션 추가

--- saladbank/src/main/java/fastcampus/saladbank/web/controller/FavoriteItemController.java
+++ saladbank/src/main/java/fastcampus/saladbank/web/controller/FavoriteItemController.java
@@ -1,7 +1,9 @@
 package fastcampus.saladbank.web.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class FavoriteItemController {
 }
