340d961f1defc76df390111cc37386287b1b2a6d
wijoonwu <devjoon@nate.com>
2022-08-26 01:27:16 +0900

    Update FavoriteItemController.java

--- saladbank/src/main/java/fastcampus/saladbank/web/controller/FavoriteItemController.java
+++ saladbank/src/main/java/fastcampus/saladbank/web/controller/FavoriteItemController.java
@@ -1,7 +1,7 @@
 package fastcampus.saladbank.web.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class FavoriteItemController {
 }
