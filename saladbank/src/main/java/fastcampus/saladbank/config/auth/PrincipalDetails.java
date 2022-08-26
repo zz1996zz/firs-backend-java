5a1254c9ec4d15d8574808efe05a468ef69c0e84
JeongSu <zz1996zz@naver.com>
2022-08-26 14:52:01 +0900

    feat: PrincipalDetails 생성

---
+++ saladbank/src/main/java/fastcampus/saladbank/config/auth/PrincipalDetails.java
@@ -0,0 +1,53 @@
+package fastcampus.saladbank.config.auth;
+
+import fastcampus.saladbank.biz.domain.Member;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+public class PrincipalDetails implements UserDetails {
+
+    private Member member;
+
+    public PrincipalDetails(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
