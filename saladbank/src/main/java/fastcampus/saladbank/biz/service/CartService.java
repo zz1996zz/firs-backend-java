e2f5d520fef191f386653b8e72e958550835f110
wijoonwu <devjoon@nate.com>
2022-08-29 16:55:51 +0900

    장바구니 추가 구현

--- saladbank/src/main/java/fastcampus/saladbank/biz/service/CartService.java
+++ saladbank/src/main/java/fastcampus/saladbank/biz/service/CartService.java
@@ -1,11 +1,7 @@
 package fastcampus.saladbank.biz.service;
 
-import fastcampus.saladbank.biz.domain.Cart;
-import fastcampus.saladbank.biz.domain.CartItem;
-import fastcampus.saladbank.biz.domain.Member;
-import fastcampus.saladbank.biz.repository.CartItemRepository;
-import fastcampus.saladbank.biz.repository.CartRepository;
-import fastcampus.saladbank.biz.repository.MemberRepository;
+import fastcampus.saladbank.biz.domain.*;
+import fastcampus.saladbank.biz.repository.*;
 import fastcampus.saladbank.web.dto.MemberForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,9 +16,10 @@ import java.util.Optional;
 public class CartService {
 
     private final CartRepository cartRepository;
+    private final CardRepository cardRepository;
     private final CartItemRepository cartItemRepository;
     private final MemberRepository memberRepository;
-
+    private final LoanRepository loanRepository;
     //장바구니 조회
     public List<CartItem> getCarts(MemberForm reqMember) {
         String username = reqMember.getUsername();
@@ -34,7 +31,32 @@ public class CartService {
 
     //장바구니 추가
     @Transactional
-    public void insertCart() {
+    public CartItem insertLoan(MemberForm reqMember, long loanId) {
+        //member 찾기
+        String username = reqMember.getUsername();
+        Optional<Member> member = memberRepository.findByUsername(username);
+        //cart 찾기
+        Cart cart = cartRepository.findByMember(member);
+        //loan 찾기
+
+        Loan loan = loanRepository.findById(loanId).orElse(null);
+        CartItem cartItem = new CartItem(cart,loan);
+        cartItemRepository.save(cartItem);
+        return cartItem;
+    }
+
+    @Transactional
+    public CartItem insertCard(MemberForm reqMember, long cardId) {
+        //member 찾기
+        String username = reqMember.getUsername();
+        Optional<Member> member = memberRepository.findByUsername(username);
+        //cart 찾기
+        Cart cart = cartRepository.findByMember(member);
+        //card 찾기
+        Card card = cardRepository.findById(cardId).orElse(null);
+        CartItem cartItem = new CartItem(cart,card);
+        cartItemRepository.save(cartItem);
+        return cartItem;
     }
 
     //장바구니 삭제
--- saladbank/src/main/java/fastcampus/saladbank/web/controller/CartController.java
+++ saladbank/src/main/java/fastcampus/saladbank/web/controller/CartController.java
@@ -1,7 +1,8 @@
 package fastcampus.saladbank.web.controller;
 
-import fastcampus.saladbank.biz.domain.Member;
-import fastcampus.saladbank.biz.repository.CartItemRepository;
+import fastcampus.saladbank.biz.domain.Card;
+import fastcampus.saladbank.biz.domain.CartItem;
+import fastcampus.saladbank.biz.domain.Loan;
 import fastcampus.saladbank.biz.service.CartService;
 import fastcampus.saladbank.web.dto.MemberForm;
 import lombok.RequiredArgsConstructor;
@@ -17,14 +18,21 @@ public class CartController {
 
     //장바구니 조회
     @GetMapping
-    public void getCarts(MemberForm reqMember ){
+    public void getCarts(@RequestBody MemberForm reqMember ){
         cartService.getCarts(reqMember);
     }
 
     //장바구니 추가
-    @PostMapping
-    public void insertCart(){
-        cartService.insertCart();
+    @PostMapping("/card")
+    public CartItem insertCard(@RequestBody MemberForm reqMember, long cardId){
+        CartItem cartItem = cartService.insertCard(reqMember, cardId);
+        return cartItem;
+    }
+
+    @PostMapping("/loan")
+    public CartItem insertLoan(@RequestBody MemberForm reqMember, long loanId){
+        CartItem cartItem = cartService.insertLoan(reqMember, loanId);
+        return cartItem;
     }
 
     //장바구니 취소
