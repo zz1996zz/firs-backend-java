b34d7b07eda82953c5d816f4a935a932fc4d7113
JeongSu <zz1996zz@naver.com>
2022-08-29 19:34:12 +0900

    feat: argumentResolver를 사용하여 DI 받기

---
+++ saladbank/src/main/java/fastcampus/saladbank/web/argumentresolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,37 @@
+package fastcampus.saladbank.web.argumentresolver;
+
+import fastcampus.saladbank.biz.domain.Member;
+import fastcampus.saladbank.config.auth.PrincipalDetails;
+import fastcampus.saladbank.web.dto.MemberForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Slf4j
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
+        boolean hasMemberFormType = MemberForm.class.isAssignableFrom(parameter.getParameterType());
+
+        return hasLoginAnnotation && hasMemberFormType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PrincipalDetails principal = (PrincipalDetails) authentication.getPrincipal();
+        String username = principal.getMember().getUsername();
+        if (username == null) {
+            return null;
+        }
+        MemberForm memberForm = new MemberForm();
+        memberForm.setUsername(username);
+        return memberForm;
+    }
+}
