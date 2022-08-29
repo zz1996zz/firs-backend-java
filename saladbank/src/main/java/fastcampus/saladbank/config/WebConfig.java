84cf0529aff7b7af4fad8a29f7c566e3166ecdff
JeongSu <zz1996zz@naver.com>
2022-08-29 20:58:34 +0900

    Update WebConfig.java

--- saladbank/src/main/java/fastcampus/saladbank/config/WebConfig.java
+++ saladbank/src/main/java/fastcampus/saladbank/config/WebConfig.java
@@ -4,6 +4,7 @@ import fastcampus.saladbank.web.argumentresolver.LoginMemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -22,5 +23,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentResolver());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
     }
 }
