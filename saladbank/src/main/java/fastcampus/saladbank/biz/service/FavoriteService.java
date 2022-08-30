537ac6aaffe5f2d1930f0ab93d0f86170f9e5007
wijoonwu <devjoon@nate.com>
2022-08-30 18:06:41 +0900

    관심상품 추가 삭제 구현

--- saladbank/src/main/java/fastcampus/saladbank/biz/service/FavoriteService.java
+++ saladbank/src/main/java/fastcampus/saladbank/biz/service/FavoriteService.java
@@ -1,15 +1,14 @@
 package fastcampus.saladbank.biz.service;
 
 import fastcampus.saladbank.biz.domain.*;
-import fastcampus.saladbank.biz.repository.CardRepository;
-import fastcampus.saladbank.biz.repository.FavoriteItemRepository;
-import fastcampus.saladbank.biz.repository.FavoriteRepository;
-import fastcampus.saladbank.biz.repository.MemberRepository;
-import fastcampus.saladbank.web.dto.CardForm;
+import fastcampus.saladbank.biz.repository.*;
+import fastcampus.saladbank.web.argumentresolver.Login;
 import fastcampus.saladbank.web.dto.MemberForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,18 +22,18 @@ public class FavoriteService {
     private final FavoriteItemRepository favoriteItemRepository;
     private final MemberRepository memberRepository;
     private final CardRepository cardRepository;
+    private final LoanRepository loanRepository;
 
-    //관심상품 삭제
+    //관심상품 전체삭제
     @Transactional
-    public void deleteFavorite(long id) {
-        Optional<Favorite> favorite = favoriteRepository.findById(id);
-        favoriteItemRepository.deleteAllByFavorite(favorite);
+    public void deleteFavorite(MemberForm memberForm) {
+        String username = memberForm.getUsername();
+        Optional<Member> member = memberRepository.findByUsername(username);
+        Favorite favorite = favoriteRepository.findByMember(member);
+        favoriteItemRepository.deleteAllByFavorite(Optional.ofNullable(favorite));
     }
 
-    //관심상품 추가
-    @Transactional
-    public void insertFavorite() {
-    }
+
     //관심상품 조회
     public List<FavoriteItem> getFavorites(MemberForm reqMember) {
         String username = reqMember.getUsername();
@@ -45,13 +44,55 @@ public class FavoriteService {
 
     //관심상품 추가 (카드)
     @Transactional
-    public FavoriteItem insertFavoriteCard(MemberForm reqMember, CardForm reqCard) {
+    public FavoriteItem insertFavoriteCard(MemberForm reqMember, long cardId) {
         String username = reqMember.getUsername();
         Optional<Member> member = memberRepository.findByUsername(username);
         Favorite favorite = favoriteRepository.findByMember(member);
-        Card card = cardRepository.findByCardName(reqCard.getCardName());
+        Card card = cardRepository.findById(cardId).orElse(null);
         FavoriteItem favoriteItem = new FavoriteItem(favorite,card);
         return favoriteItemRepository.save(favoriteItem);
 
+    }
+
+    //관심상품 추가 (카드)
+    @Transactional
+    public FavoriteItem insertFavoriteLoan(MemberForm reqMember, long loanId) {
+        String username = reqMember.getUsername();
+        Optional<Member> member = memberRepository.findByUsername(username);
+        Favorite favorite = favoriteRepository.findByMember(member);
+        Loan loan = loanRepository.findById(loanId).orElse(null);
+        FavoriteItem favoriteItem = new FavoriteItem(favorite,loan);
+        return favoriteItemRepository.save(favoriteItem);
+
+    }
+    //관심상품 삭제(카드)
+    @Transactional
+
+    public void deleteCartCard(MemberForm memberForm, long id) {
+        String username = memberForm.getUsername();
+        Optional<Member> member = memberRepository.findByUsername(username);
+        Favorite favorite = favoriteRepository.findByMember(member);
+        List<FavoriteItem> FavoriteItemList = favoriteRepository.findAllByFavorite(favorite);
+        Optional<Card> card = cardRepository.findById(id);
+        for(FavoriteItem favoriteItem : FavoriteItemList){
+            if(favoriteItem.getCardList().contains(card.get())){
+                favoriteRepository.deleteById(favoriteItem.getFavoriteItemId());
+            }
+        }
+    }
+
+    //관심상품 삭제(대출)
+    @Transactional
+    public void deleteCartLoan(MemberForm memberForm, long id) {
+        String username = memberForm.getUsername();
+        Optional<Member> member = memberRepository.findByUsername(username);
+        Favorite favorite = favoriteRepository.findByMember(member);
+        List<FavoriteItem> FavoriteItemList = favoriteRepository.findAllByFavorite(favorite);
+        Optional<Loan> loan = loanRepository.findById(id);
+        for(FavoriteItem favoriteItem : FavoriteItemList){
+            if(favoriteItem.getLoanList().contains(loan.get())){
+                favoriteRepository.deleteById(favoriteItem.getFavoriteItemId());
+            }
+        }
     }
 }
--- saladbank/src/main/java/fastcampus/saladbank/web/controller/FavoriteController.java
+++ saladbank/src/main/java/fastcampus/saladbank/web/controller/FavoriteController.java
@@ -1,11 +1,15 @@
 package fastcampus.saladbank.web.controller;
 
 import fastcampus.saladbank.biz.domain.FavoriteItem;
+import fastcampus.saladbank.biz.domain.Member;
 import fastcampus.saladbank.biz.service.FavoriteService;
-import fastcampus.saladbank.web.dto.CardForm;
+import fastcampus.saladbank.web.argumentresolver.Login;
 import fastcampus.saladbank.web.dto.MemberForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,24 +18,44 @@ public class FavoriteController {
 
     private final FavoriteService favoriteService;
 
-    //관심상품 등록
+    //관심상품 등록(카드)
     @PostMapping("/card")
-    public FavoriteItem insertFavoriteCard(@RequestBody MemberForm reqMember,
-                                           @RequestBody CardForm reqCard){
-        return favoriteService.insertFavoriteCard(reqMember,reqCard);
+    public FavoriteItem insertFavoriteCard(@Login MemberForm reqMember,
+                                           @RequestBody Map<String,Long> map ){
+        return favoriteService.insertFavoriteCard(reqMember, map.get("card_id"));
     }
 
-
-    //관심상품 해지
-    @DeleteMapping("/{id}")
-    public void deleteFavorite(@PathVariable long id){
-        favoriteService.deleteFavorite(id);
+    //관심상품 등록(카드)
+    @PostMapping("/loan")
+    public FavoriteItem insertFavoriteLoan(@Login MemberForm reqMember,
+                                           @RequestBody Map<String,Long> map ){
+        return favoriteService.insertFavoriteLoan(reqMember, map.get("loan_id"));
     }
 
+    //관심상품 삭제(전체)
+    @DeleteMapping()
+    public void deleteFavorite(@Login MemberForm memberForm){
+        favoriteService.deleteFavorite(memberForm);
+    }
+
+    //관심상품 삭제(카드)
+    @DeleteMapping("/card/{id}")
+    public void deleteFavoriteCard(@Login MemberForm memberForm,
+                                   @PathVariable long id){
+        favoriteService.deleteCartCard(memberForm,id);
+    }
+
+    //관심상품 삭제(카드)
+    @DeleteMapping("/loan/{id}")
+    public void deleteFavoriteLoan(@Login MemberForm memberForm,
+                                   @PathVariable long id){
+        favoriteService.deleteCartLoan(memberForm,id);
+    }
     //관심상품 조회
     @GetMapping
-    public void getFavorites(MemberForm reqMember){
-        favoriteService.getFavorites(reqMember);
+    public List<FavoriteItem> getFavorites(MemberForm reqMember){
+        return favoriteService.getFavorites(reqMember);
     }
+
 
 }
