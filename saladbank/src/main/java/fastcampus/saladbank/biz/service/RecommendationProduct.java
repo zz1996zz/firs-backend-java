d2842692738f768741630e5a30158a82486f293d
JeongSu <53508659+zz1996zz@users.noreply.github.com>
2022-08-30 23:47:56 +0900

    feat: 회원 추천 상품 출력 기능 구현 (#52)

---
+++ saladbank/src/main/java/fastcampus/saladbank/biz/service/RecommendationProduct.java
@@ -0,0 +1,78 @@
+package fastcampus.saladbank.biz.service;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import fastcampus.saladbank.biz.domain.Card;
+import fastcampus.saladbank.biz.domain.Loan;
+import fastcampus.saladbank.biz.domain.Member;
+import fastcampus.saladbank.biz.repository.CardRepository;
+import fastcampus.saladbank.biz.repository.LoanRepository;
+import fastcampus.saladbank.biz.repository.MemberRepository;
+import fastcampus.saladbank.config.LocalDateTimeSerializer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class RecommendationProduct {
+
+    private final MemberRepository memberRepository;
+    private final LoanRepository loanRepository;
+    private final CardRepository cardRepository;
+
+    public String setInterestRateAndLimit(String username) {
+        Member findMember = memberRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("멤버를 찾지 못했습니다."));
+        StringBuilder sb = new StringBuilder();
+        int income = findMember.getIncome();
+        sb.append(income + " ");
+        if (findMember.getHouse()) {
+            double v = ((income * 1.32) / 1000) + 2;
+            sb.append(v);
+        } else {
+            double v = ((income * 1.32) / 1000);
+            sb.append(v);
+        }
+        return sb.toString();
+    }
+
+    public String getRecommendationLoan(String username) {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeSerializer());
+        Gson gson = gsonBuilder.setPrettyPrinting().create();
+        List<Loan> filterLoan = new ArrayList<>();
+        Member findMember = memberRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("멤버를 찾지 못했습니다."));
+        List<Loan> loanAll = loanRepository.findAll();
+        for (Loan loan : loanAll) {
+            if (Integer.parseInt(loan.getCreditLine()) <= findMember.getIncome()) {
+                filterLoan.add(loan);
+            }
+        }
+        if (!filterLoan.isEmpty()) {
+            return gson.toJson(filterLoan);
+
+        }
+        return null;
+    }
+
+    public String getRecommendationCard(String username) {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeSerializer());
+        Gson gson = gsonBuilder.setPrettyPrinting().create();
+        List<Card> filterCard = new ArrayList<>();
+        Member findMember = memberRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("멤버를 찾지 못했습니다."));
+        List<Card> cardAll = cardRepository.findAll();
+        for (Card card : cardAll) {
+            if (card.getTag().contains(findMember.getJob())) {
+                filterCard.add(card);
+            }
+        }
+        if (!filterCard.isEmpty()) {
+            return gson.toJson(filterCard);
+        }
+        return null;
+    }
+}
---
+++ saladbank/src/main/java/fastcampus/saladbank/config/LocalDateTimeSerializer.java
@@ -0,0 +1,20 @@
+package fastcampus.saladbank.config;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeSerializer implements JsonSerializer<LocalDateTime> {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Override
+    public JsonElement serialize(LocalDateTime src, Type typeOfSrc, JsonSerializationContext context) {
+        return new JsonPrimitive(formatter.format(src));
+    }
+}
