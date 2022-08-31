5583ef3b36d4e66d0f6e1eeaec27e8c567e19cfa
JeongSu <53508659+zz1996zz@users.noreply.github.com>
2022-09-01 02:03:39 +0900

    Feat get card list (#57)
    * feat: 화이트 리스트에 상품 전체 조회 추가
    
    * feat: 카드 상품만 조회하는 기능 구현

--- saladbank/src/main/java/fastcampus/saladbank/biz/service/LoanService.java
+++ saladbank/src/main/java/fastcampus/saladbank/biz/service/LoanService.java
@@ -1,7 +1,11 @@
 package fastcampus.saladbank.biz.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class LoanService {
 }
