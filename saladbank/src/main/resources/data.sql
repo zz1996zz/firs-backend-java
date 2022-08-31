58b1131b74658ab888123705b69868f830504e88
JeongSu <zz1996zz@naver.com>
2022-08-31 16:12:16 +0900

    feat: 더미 데이터 추가

--- saladbank/src/main/resources/data.sql
+++ saladbank/src/main/resources/data.sql
@@ -10,10 +10,28 @@ INSERT INTO card (PRODUCT_TYPE, CARD_NAME, CARD_COMPANY, ANNUAL_FEE, CARD_TYPE, 
 VALUES ("카드", "청춘대로 톡톡카드", "KB국민카드", "12000", "신용", "Simple하게 즐기자! 혜택 톡톡!", "1.2%", "20만원 당 7천 머니", "5만원 당 3천원", "7만원 당 4천 머니", "1000", "무직 독서 40대", "https://cdn.banksalad.com/cards/kb_3350.png");
 
 INSERT INTO card (PRODUCT_TYPE, CARD_NAME, CARD_COMPANY, ANNUAL_FEE, CARD_TYPE, CARD_DESCRIPTION, FRANCHISEE, SHOPPING, OILING, INSURANCE, CAFE, TAG, IMG)
+VALUES ("카드", "American Express® Green", "삼성카드", "49000", "신용", "해외는 실속있게, 일상은 실용적으로", "1.2%", "20만원 당 7천 머니", "5만원 당 3천원", "7만원 당 4천 머니", "1000", "주부 낚시 40대", "https://cdn.banksalad.com/entities/etc/1525243451036-1279.png");
+
+INSERT INTO card (PRODUCT_TYPE, CARD_NAME, CARD_COMPANY, ANNUAL_FEE, CARD_TYPE, CARD_DESCRIPTION, FRANCHISEE, SHOPPING, OILING, INSURANCE, CAFE, TAG, IMG)
+VALUES ("카드", "Oil & Life카드(Oil)", "IBK기업은행", "5000", "신용", "GS칼텍스 리터당 120원 할인에 Prime 서비스 추가 시엔 다양한 무료쿠폰 또는 차량정비 서비스 제공", "1.2%", "20만원 당 7천 머니", "5만원 당 3천원", "7만원 당 4천 머니", "1000", "직장인 운동 50대", "https://cdn.banksalad.com/cards/ibk_cr0000000286.png");
+
+INSERT INTO card (PRODUCT_TYPE, CARD_NAME, CARD_COMPANY, ANNUAL_FEE, CARD_TYPE, CARD_DESCRIPTION, FRANCHISEE, SHOPPING, OILING, INSURANCE, CAFE, TAG, IMG)
 VALUES ("카드", "BC 바로 클리어 플러스", "BC 바로카드", "5000", "체크", "점심/배달앱 7% 할인, 쿠팡 10% 할인", "0.4-2.0%", "10만원 당 5천 머니", "5만원 당 2천원", "7만원 당 2천 머니", "1000", "직장인 게임 60대", "https://cdn.banksalad.com/resources/images/cards/bc_clear.png");
 
 INSERT INTO card (PRODUCT_TYPE, CARD_NAME, CARD_COMPANY, ANNUAL_FEE, CARD_TYPE, CARD_DESCRIPTION, FRANCHISEE, SHOPPING, OILING, INSURANCE, CAFE, TAG, IMG)
 VALUES ("카드", "LOCA LIKIT 1.2", "롯데카드", "10000", "체크", "모든 가맹점 1.2% 할인", "0.3-1.0%", "10만원 당 5천 머니", "5만원 당 3천원", "2만원 당 1천 머니", "1000", "주부 음악 감상 50대", "https://cdn.banksalad.com/resources/images/cards/lotte_loca_12.png");
+
+INSERT INTO card (PRODUCT_TYPE, CARD_NAME, CARD_COMPANY, ANNUAL_FEE, CARD_TYPE, CARD_DESCRIPTION, FRANCHISEE, SHOPPING, OILING, INSURANCE, CAFE, TAG, IMG)
+VALUES ("카드", "누리체크카드", "KB국민카드", "0", "체크", "집중된 할인보다 폭넓은 할인을 원하는 당신께. 모든 가맹점에서 할인되는 체크카드", "10만원 당 5천 머니", "5만원 당 3천원", "2만원 당 1천 머니", "1000", "무직 음악감상 70대이상", "https://cdn.banksalad.com/cards/509.png");
+
+INSERT INTO card (PRODUCT_TYPE, CARD_NAME, CARD_COMPANY, ANNUAL_FEE, CARD_TYPE, CARD_DESCRIPTION, FRANCHISEE, SHOPPING, OILING, INSURANCE, CAFE, TAG, IMG)
+VALUES ("카드", "프렌즈 체크카드", "카카오뱅크", "0", "체크", "귀여운 프렌즈와 좋은 혜택!", "10만원 당 5천 머니", "5만원 당 3천원", "2만원 당 1천 머니", "1000", "직장인 등산 20대", "https://cdn.banksalad.com/cards/3596.png");
+
+INSERT INTO card (PRODUCT_TYPE, CARD_NAME, CARD_COMPANY, ANNUAL_FEE, CARD_TYPE, CARD_DESCRIPTION, FRANCHISEE, SHOPPING, OILING, INSURANCE, CAFE, TAG, IMG)
+VALUES ("카드", "K bank 체크카드 포인트적립형", "케이뱅크", "0", "체크", "1포인트 당 1원으로 적립!", "10만원 당 5천 머니", "5만원 당 3천원", "2만원 당 1천 머니", "1000", "직장인 게임 30대", "https://cdn.banksalad.com/cards/kbank_3424.png");
+
+
+
 
 INSERT INTO loan (PRODUCT_TYPE, LOAN_NAME, LOAN_COMPANY, CREDIT_LINE, RATE, PRIME_RATE, PERIOD, LOAN_LINE, TAG, IMG)
 VALUES ("대출", "보험계약대출", "교보생명", "2000", "2.82 10.5", false, 3, "50 95", "직장인 모바일 2금융권", "https://cdn.banksalad.com/graphic/color/logo/circle/kyobo.png");
@@ -30,3 +48,17 @@ VALUES ("대출", "JB 탄탄대로 사업자대출", "전북은행", "5000", "7.
 INSERT INTO loan (PRODUCT_TYPE, LOAN_NAME, LOAN_COMPANY, CREDIT_LINE, RATE, PRIME_RATE, PERIOD, LOAN_LINE, TAG, IMG)
 VALUES ("대출", "FI신용대출", "다올저축은행", "1000", "5.9 19.9", false, 3, "50 90", "주부 모바일 2금융권", "https://cdn.banksalad.com/graphic/color/logo/circle/daol.png");
 
+INSERT INTO loan (PRODUCT_TYPE, LOAN_NAME, LOAN_COMPANY, CREDIT_LINE, RATE, PRIME_RATE, PERIOD, LOAN_LINE, TAG, IMG)
+VALUES ("대출", "HB니즈론", "HB저축은행", "2000", "4.81 9.99", false, 2, "50 90", "무직 모바일 2금융권", "https://cdn.banksalad.com/graphic/color/logo/circle/hb.png");
+
+INSERT INTO loan (PRODUCT_TYPE, LOAN_NAME, LOAN_COMPANY, CREDIT_LINE, RATE, PRIME_RATE, PERIOD, LOAN_LINE, TAG, IMG)
+VALUES ("대출", "신용대출", "롯데캐피탈", "2500", "5.4 18.9", false, 2, "50 90", "주부 모바일 1금융권", "https://cdn.banksalad.com/graphic/color/logo/circle/lotte.png");
+
+INSERT INTO loan (PRODUCT_TYPE, LOAN_NAME, LOAN_COMPANY, CREDIT_LINE, RATE, PRIME_RATE, PERIOD, LOAN_LINE, TAG, IMG)
+VALUES ("대출", "BNK모바일신용대출", "경남은행", "700", "3.6 6.66", false, 1, "50 90", "직장인 모바일 2금융권", "https://cdn.banksalad.com/graphic/color/logo/circle/bnk-kyongnam.png");
+
+INSERT INTO loan (PRODUCT_TYPE, LOAN_NAME, LOAN_COMPANY, CREDIT_LINE, RATE, PRIME_RATE, PERIOD, LOAN_LINE, TAG, IMG)
+VALUES ("대출", "직장인행복대출", "NH저축은행", "5000", "5.5 9.4", false, 3, "50 90", "직장인 모바일 1금융권", "https://cdn.banksalad.com/graphic/color/logo/circle/nh.png");
+
+INSERT INTO loan (PRODUCT_TYPE, LOAN_NAME, LOAN_COMPANY, CREDIT_LINE, RATE, PRIME_RATE, PERIOD, LOAN_LINE, TAG, IMG)
+VALUES ("대출", "애드론 신용대출", "스마트저축은행", "6000", "5.9 19.9", false, 1, "50 90", "주부 모바일 2금융권", "https://cdn.banksalad.com/graphic/color/logo/circle/smartbank.png");
