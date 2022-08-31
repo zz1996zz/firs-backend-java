1296f9f6f17ae616a02bbbc04823a49d3d126f19
JeongSu <zz1996zz@naver.com>
2022-09-01 02:07:51 +0900

    feat: 대출 상품만 조회하는 기능 구현

--- saladbank/src/main/java/fastcampus/saladbank/biz/service/LoanService.java
+++ saladbank/src/main/java/fastcampus/saladbank/biz/service/LoanService.java
@@ -1,11 +1,20 @@
 package fastcampus.saladbank.biz.service;
 
+import fastcampus.saladbank.biz.repository.LoanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class LoanService {
+
+    private final LoanRepository loanRepository;
+
+    public List getLoanList() {
+        return loanRepository.findAll();
+    }
 }
--- saladbank/src/main/java/fastcampus/saladbank/web/controller/LoanController.java
+++ saladbank/src/main/java/fastcampus/saladbank/web/controller/LoanController.java
@@ -1,11 +1,22 @@
 package fastcampus.saladbank.web.controller;
 
+import fastcampus.saladbank.biz.service.LoanService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
+import java.util.List;
+
 @RestController
 @RequestMapping("/loans")
+@RequiredArgsConstructor
 public class LoanController {
+
+    private final LoanService loanService;
+
+    @GetMapping
+    public List getLoanList() {
+        return loanService.getLoanList();
+    }
 }
