5c696f2456c87e2c4e6adc7128941d58fd7c5278
JeongSu <zz1996zz@naver.com>
2022-08-31 14:57:06 +0900

    feat: 카드 더미데이터 수정

--- saladbank/src/main/resources/data.sql
+++ saladbank/src/main/resources/data.sql
@@ -1,19 +1,19 @@
 SET FOREIGN_KEY_CHECKS=0;
 
 INSERT INTO card (PRODUCT_TYPE, CARD_NAME, CARD_COMPANY, ANNUAL_FEE, CARD_TYPE, CARD_DESCRIPTION, FRANCHISEE, SHOPPING, OILING, INSURANCE, CAFE, TAG, IMG)
-VALUES ("카드", "신한카드", "신한", "10000", "신용", "매일매일 할인 좋아", "0.3 1.0", "10만원 당 5천 머니", "5만원 당 3천원", "7만원 당 2천 머니", "100", "주부 그림/운동 30대");
+VALUES ("카드", "신한카드 YOLO(미니언즈)", "신한카드", "15000", "신용", "인율과 디자인을 내 마음대로!! 나의 맞춤카드를 원하신다면, “YOLO”오세요~", "2.0%", "10만원 당 5천 머니", "5만원 당 3천원", "7만원 당 2천 머니", "1000", "주부 낚시 30대", "https://cdn.banksalad.com/entities/etc/1556265601126-3858.png");
 
 INSERT INTO card (PRODUCT_TYPE, CARD_NAME, CARD_COMPANY, ANNUAL_FEE, CARD_TYPE, CARD_DESCRIPTION, FRANCHISEE, SHOPPING, OILING, INSURANCE, CAFE, TAG, IMG)
-VALUES ("카드", "국민카드", "국민", "10000", "체크", "매일매일 할인 안좋아", "0.3 2.0", "10만원 당 5천 머니", "5만원 당 3천원", "7만원 당 2천 머니", "100", "직장인 그림/운동 30대");
+VALUES ("카드", "삼성카드 taptap O", "삼성카드", "10000", "신용", "직접 선택하는 혜택, 탭탭오 옵션형!", "0.3-2.0%", "5만원 당 2천 머니", "3만원 당 1천원", "7만원 당 2천 머니", "1000", "직장인 운동 20대", "https://cdn.banksalad.com/cards/samsung_2928.png");
 
 INSERT INTO card (PRODUCT_TYPE, CARD_NAME, CARD_COMPANY, ANNUAL_FEE, CARD_TYPE, CARD_DESCRIPTION, FRANCHISEE, SHOPPING, OILING, INSURANCE, CAFE, TAG, IMG)
-VALUES ("카드", "농협카드", "농협", "10000", "체크", "매일매일 할인 개좋아", "0.3 1.0", "10만원 당 5천 머니", "5만원 당 3천원", "7만원 당 2천 머니", "100", "무직 그림/운동 30대");
+VALUES ("카드", "청춘대로 톡톡카드", "KB국민카드", "12000", "신용", "Simple하게 즐기자! 혜택 톡톡!", "1.2%", "20만원 당 7천 머니", "5만원 당 3천원", "7만원 당 4천 머니", "1000", "무직 독서 40대", "https://cdn.banksalad.com/cards/kb_3350.png");
 
 INSERT INTO card (PRODUCT_TYPE, CARD_NAME, CARD_COMPANY, ANNUAL_FEE, CARD_TYPE, CARD_DESCRIPTION, FRANCHISEE, SHOPPING, OILING, INSURANCE, CAFE, TAG, IMG)
-VALUES ("카드", "우리카드", "우리", "10000", "신용", "매일매일 할인 넘나좋아", "0.3 1.0", "10만원 당 5천 머니", "5만원 당 3천원", "7만원 당 2천 머니", "100", "주부 그림/운동 30대");
+VALUES ("카드", "BC 바로 클리어 플러스", "BC 바로카드", "5000", "체크", "점심/배달앱 7% 할인, 쿠팡 10% 할인", "0.4-2.0%", "10만원 당 5천 머니", "5만원 당 2천원", "7만원 당 2천 머니", "1000", "직장인 게임 60대", "https://cdn.banksalad.com/resources/images/cards/bc_clear.png");
 
 INSERT INTO card (PRODUCT_TYPE, CARD_NAME, CARD_COMPANY, ANNUAL_FEE, CARD_TYPE, CARD_DESCRIPTION, FRANCHISEE, SHOPPING, OILING, INSURANCE, CAFE, TAG, IMG)
-VALUES ("카드", "기업카드", "기업", "10000", "신용", "매일매일 할인 좋아", "0.3 1.0", "10만원 당 5천 머니", "5만원 당 3천원", "7만원 당 2천 머니", "100", "주부 그림/운동 30대");
+VALUES ("카드", "LOCA LIKIT 1.2", "롯데카드", "10000", "체크", "모든 가맹점 1.2% 할인", "0.3-1.0%", "10만원 당 5천 머니", "5만원 당 3천원", "2만원 당 1천 머니", "1000", "주부 음악 감상 50대", "https://cdn.banksalad.com/resources/images/cards/lotte_loca_12.png");
 
 INSERT INTO loan (PRODUCT_TYPE, LOAN_NAME, LOAN_COMPANY, CREDIT_LINE, RATE, PRIME_RATE, PERIOD, LOAN_LINE, TAG, IMG)
 VALUES ("대출", "보험계약대출", "교보생명", "2000", "2.82 10.5", false, 3, "50 95", "직장인 모바일 2금융권", "https://cdn.banksalad.com/graphic/color/logo/circle/kyobo.png");
