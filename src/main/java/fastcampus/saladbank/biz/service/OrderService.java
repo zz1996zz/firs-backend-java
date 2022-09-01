35b32e93318ce562a3fadb81722c19a32814d053
wijoonwu <devjoon@nate.com>
2022-09-01 18:30:22 +0900

    주문신청 구현

--- src/main/java/fastcampus/saladbank/biz/service/OrderService.java
+++ src/main/java/fastcampus/saladbank/biz/service/OrderService.java
@@ -1,49 +1,65 @@
 package fastcampus.saladbank.biz.service;
 
-import fastcampus.saladbank.biz.domain.Order;
+import fastcampus.saladbank.biz.domain.*;
+import fastcampus.saladbank.biz.repository.CartRepository;
+import fastcampus.saladbank.biz.repository.MemberRepository;
 import fastcampus.saladbank.biz.repository.OrderRepository;
+import fastcampus.saladbank.web.dto.MemberForm;
 import fastcampus.saladbank.web.dto.OrderForm;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
+@org.springframework.transaction.annotation.Transactional(readOnly=true)
+@Slf4j
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final CartRepository cartRepository;
 
     // 주문 등록
     @Transactional
-    public Long save(OrderForm orderForm) {
-        return orderRepository.save(orderForm.toEntity()).getOrderId();
-    }
+    public void insertOrder(MemberForm memberForm) throws Exception {
+        //member 찾기
+        String username = memberForm.getUsername();
+        Member member = memberRepository.findByUsername(username).orElse(null);
+        Optional<Cart> cart = Optional.ofNullable(cartRepository.findByMember(member));
+        if(cart.isPresent()){
+            if(!cart.get().getCardList().isEmpty()) {
+                Order order = new Order(member,  "신청");
+                for (int i=0; i < cart.get().getCardList().size(); i++) {
+                    Card card = cart.get().getCardList().get(i);
+                    order.addOrderCard(card);
+                    orderRepository.save(order);
+                }
+            }
 
-    // 주문 수정
-    @Transactional
-    public Long update(Long id, OrderForm orderForm) {
-        Order order = orderRepository.findById(id).orElseThrow(() ->
-                new IllegalArgumentException("해당 주문이 없습니다. id:" + id));
+            if (!cart.get().getLoanList().isEmpty()){
+                Order order = new Order(member,"신청");
+                for(int i=0; i<cart.get().getLoanList().size(); i++){
+                    Loan loan = cart.get().getLoanList().get(i);
+                    order.addOrderLoan(loan);
+                    order.addExpiryDate(loan.getPeriod());
+                    orderRepository.save(order);
+                }
+            }
+        }
 
-        order.update(orderForm.getApplyDate(), orderForm.getExpiryDate(), orderForm.getStatus());
-
-        return id;
     }
 
     // 주문 조회
-    public OrderForm findById(Long id) {
-        Order entity = orderRepository.findById(id).orElseThrow(() ->
-                new IllegalArgumentException("해당 주문이 없습니다. id=" + id));
+//    public OrderForm findById(Long id) {
+//        Order entity = orderRepository.findById(id).orElseThrow(() ->
+//                new IllegalArgumentException("해당 주문이 없습니다. id=" + id));
+//
+//        return new OrderForm(entity);
+//    }
 
-        return new OrderForm(entity);
-    }
 
-    // 주문 삭제
-    @Transactional
-    public void delete(Long id) {
-        Order order = orderRepository.findById(id).orElseThrow(() ->
-                new IllegalArgumentException("해당 주문이 없습니다. id=" + id));
 
-        orderRepository.delete(order);
-    }
 }
--- src/main/java/fastcampus/saladbank/web/dto/OrderForm.java
+++ src/main/java/fastcampus/saladbank/web/dto/OrderForm.java
@@ -2,14 +2,14 @@ package fastcampus.saladbank.web.dto;
 
 import fastcampus.saladbank.biz.domain.Order;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import net.bytebuddy.asm.Advice;
 
 import java.time.LocalDate;
 
-@Getter
-@NoArgsConstructor
+@Data
 public class OrderForm {
     private LocalDate applyDate;
     private LocalDate expiryDate;
@@ -23,19 +23,19 @@ public class OrderForm {
         this.status = status;
     }
 
-    public Order toEntity() {
-        return Order.builder()
-                .applyDate(applyDate)
-                .expiryDate(expiryDate)
-                .status(status)
-                .build();
-    }
-
-    // 주문 조회시 사용
-    public OrderForm(Order entity) {
-        this.applyDate = entity.getApplyDate();
-        this.expiryDate = entity.getExpiryDate();
-        this.status = entity.getStatus();
-    }
+//    public Order toEntity() {
+//        return Order.builder()
+//                .applyDate(applyDate)
+//                .expiryDate(expiryDate)
+//                .status(status)
+//                .build();
+//    }
+//
+//    // 주문 조회시 사용
+//    public OrderForm(Order entity) {
+//        this.applyDate = entity.getApplyDate();
+//        this.expiryDate = entity.getExpiryDate();
+//        this.status = entity.getStatus();
+//    }
 
 }
