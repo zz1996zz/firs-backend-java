d2eb05e492afceb2c998eb673f0e2cba6a701465
wijoonwu <devjoon@nate.com>
2022-09-02 03:38:52 +0900

    Update OrderService.java

--- src/main/java/fastcampus/saladbank/biz/service/OrderService.java
+++ src/main/java/fastcampus/saladbank/biz/service/OrderService.java
@@ -23,34 +23,34 @@ public class OrderService {
     private final CartRepository cartRepository;
 
     // 주문 등록
-    @Transactional
-    public void insertOrder(MemberForm memberForm) throws Exception {
-        //member 찾기
-        String username = memberForm.getUsername();
-        Member member = memberRepository.findByUsername(username).orElse(null);
-        Optional<Cart> cart = Optional.ofNullable(cartRepository.findByMember(member));
-        if(cart.isPresent()){
-            if(!cart.get().getCardList().isEmpty()) {
-                Order order = new Order(member,  "신청");
-                for (int i=0; i < cart.get().getCardList().size(); i++) {
-                    Card card = cart.get().getCardList().get(i);
-                    order.addOrderCard(card);
-                    orderRepository.save(order);
-                }
-            }
-
-            if (!cart.get().getLoanList().isEmpty()){
-                Order order = new Order(member,"신청");
-                for(int i=0; i<cart.get().getLoanList().size(); i++){
-                    Loan loan = cart.get().getLoanList().get(i);
-                    order.addOrderLoan(loan);
-                    order.addExpiryDate(loan.getPeriod());
-                    orderRepository.save(order);
-                }
-            }
-        }
-
-    }
+//    @Transactional
+//    public void insertOrder(MemberForm memberForm) throws Exception {
+//        //member 찾기
+//        String username = memberForm.getUsername();
+//        Member member = memberRepository.findByUsername(username).orElse(null);
+//        Optional<Cart> cart = Optional.ofNullable(cartRepository.findByMember(member));
+//        if(cart.isPresent()){
+//            if(!cart.get().getCardList().isEmpty()) {
+//                Order order = new Order(member,  "신청");
+//                for (int i=0; i < cart.get().getCardList().size(); i++) {
+//                    Card card = cart.get().getCardList().get(i);
+//                    order.addOrderCard(card);
+//                    orderRepository.save(order);
+//                }
+//            }
+//
+//            if (!cart.get().getLoanList().isEmpty()){
+//                Order order = new Order(member,"신청");
+//                for(int i=0; i<cart.get().getLoanList().size(); i++){
+//                    Loan loan = cart.get().getLoanList().get(i);
+//                    order.addOrderLoan(loan);
+//                    order.addExpiryDate(loan.getPeriod());
+//                    orderRepository.save(order);
+//                }
+//            }
+//        }
+//
+//    }
 
     //주문 전체 조회
     public List getOrderList(String username) {
