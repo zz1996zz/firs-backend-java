2b83694977475b043d63055800eebc1885fd7a02
JeongSu <zz1996zz@naver.com>
2022-09-02 16:27:29 +0900

    마이페이지 수정

--- src/main/java/fastcampus/saladbank/biz/service/OrderService.java
+++ src/main/java/fastcampus/saladbank/biz/service/OrderService.java
@@ -1,18 +1,20 @@
 package fastcampus.saladbank.biz.service;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import fastcampus.saladbank.biz.domain.Card;
 import fastcampus.saladbank.biz.domain.Loan;
 import fastcampus.saladbank.biz.domain.Member;
 import fastcampus.saladbank.biz.repository.CardRepository;
 import fastcampus.saladbank.biz.repository.LoanRepository;
 import fastcampus.saladbank.biz.repository.MemberRepository;
-import fastcampus.saladbank.biz.repository.OrderRepository;
+import fastcampus.saladbank.config.LocalDateTimeSerializer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +23,6 @@ import java.util.List;
 @Transactional(readOnly = true)
 @Slf4j
 public class OrderService {
-    private final OrderRepository orderRepository;
     private final MemberRepository memberRepository;
     private final CardRepository cardRepository;
     private final LoanRepository loanRepository;
@@ -41,7 +42,9 @@ public class OrderService {
     }
 
     public String getCardList() {
-        Gson gson = new Gson();
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeSerializer());
+        Gson gson = gsonBuilder.setPrettyPrinting().create();
         List<Card> cardList = new ArrayList<>();
         cardList.add(cardRepository.findById(1L).orElseThrow(() -> new RuntimeException("해당 상품이 없습니다.")));
         cardList.add(cardRepository.findById(7L).orElseThrow(() -> new RuntimeException("해당 상품이 없습니다.")));
@@ -50,7 +53,9 @@ public class OrderService {
     }
 
     public String getLoanList() {
-        Gson gson = new Gson();
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeSerializer());
+        Gson gson = gsonBuilder.setPrettyPrinting().create();
         List<Loan> loanList = new ArrayList<>();
         loanList.add(loanRepository.findById(3L).orElseThrow(() -> new RuntimeException("해당 상품이 없습니다.")));
         loanList.add(loanRepository.findById(5L).orElseThrow(() -> new RuntimeException("해당 상품이 없습니다.")));
