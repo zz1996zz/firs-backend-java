8e812793ca4434da0d6a1e10257a5db3feb40c2a
JeongSu <zz1996zz@naver.com>
2022-09-02 21:27:09 +0900

    repo 수정

--- src/main/java/fastcampus/saladbank/biz/repository/LoanRepository.java
+++ src/main/java/fastcampus/saladbank/biz/repository/LoanRepository.java
@@ -9,9 +9,9 @@ import java.util.List;
 
 public interface LoanRepository extends JpaRepository<Loan, Long> {
 
-    @Query(value = "SELECT l.loan_id, l.createdDate, l.modifiedDate, l.product_type, l.loan_name, l.loan_company, l.credit_line, l.rate, l.prime_rate, l.period, l.loan_line, l.tag, l.img FROM Loan l WHERE l.loan_name LIKE %:keyword% OR l.loan_company LIKE %:keyword% OR l.tag LIKE %:keyword%", nativeQuery = true)
+    @Query(value = "SELECT l.loan_id, l.createdDate, l.modifiedDate, l.product_type, l.loan_name, l.loan_company, l.credit_line, l.rate, l.prime_rate, l.period, l.loan_line, l.tag, l.img FROM LOAN l WHERE l.loan_name LIKE %:keyword% OR l.loan_company LIKE %:keyword% OR l.tag LIKE %:keyword%", nativeQuery = true)
     List<Loan> findAllByLoanInfo(@Param("keyword") String keyword);
 
-    @Query(value = "SELECT l.loan_id, l.createdDate, l.modifiedDate, l.product_type, l.loan_name, l.loan_company, l.credit_line, l.rate, l.prime_rate, l.period, l.loan_line, l.tag, l.img FROM Loan l", nativeQuery = true)
+    @Query(value = "SELECT l.loan_id, l.createdDate, l.modifiedDate, l.product_type, l.loan_name, l.loan_company, l.credit_line, l.rate, l.prime_rate, l.period, l.loan_line, l.tag, l.img FROM LOAN l", nativeQuery = true)
     List<Loan> findLoanAll();
 }
