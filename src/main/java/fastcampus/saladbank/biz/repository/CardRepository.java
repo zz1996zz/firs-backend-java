952ade2df50e3913c94e870dfc19ca6851d6b6ae
JeongSu <zz1996zz@naver.com>
2022-09-03 02:35:25 +0900

    코드 리팩토링

--- src/main/java/fastcampus/saladbank/biz/repository/CardRepository.java
+++ src/main/java/fastcampus/saladbank/biz/repository/CardRepository.java
@@ -2,18 +2,10 @@ package fastcampus.saladbank.biz.repository;
 
 import fastcampus.saladbank.biz.domain.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CardRepository extends JpaRepository<Card,Long> {
-
-    @Query(value = "SELECT CARD.card_id, CARD.createdDate, CARD.modifiedDate, CARD.PRODUCT_TYPE, CARD.CARD_NAME, CARD.CARD_COMPANY, CARD.ANNUAL_FEE, CARD.CARD_TYPE, CARD.CARD_DESCRIPTION, CARD.FRANCHISEE, CARD.SHOPPING, CARD.OILING, CARD.INSURANCE, CARD.CAFE, CARD.TAG, CARD.IMG FROM CARD WHERE CARD.CARD_NAME LIKE %:keyword% OR CARD.CARD_COMPANY LIKE %:keyword% OR CARD.TAG LIKE %:keyword%", nativeQuery = true)
-    List<Card> findAllByCardInfo(@Param("keyword") String keyword);
-
-    @Query(value = "SELECT CARD.card_id, CARD.createdDate, CARD.modifiedDate, CARD.PRODUCT_TYPE, CARD.CARD_NAME, CARD.CARD_COMPANY, CARD.ANNUAL_FEE, CARD.CARD_TYPE, CARD.CARD_DESCRIPTION, CARD.FRANCHISEE, CARD.SHOPPING, CARD.OILING, CARD.INSURANCE, CARD.CAFE, CARD.TAG, CARD.IMG FROM CARD", nativeQuery = true)
-    List<Card> findCardAll();
 
     List<Card> findAllByCardNameContainsOrCardCompanyContainsOrTagContains(String keyword1, String keyword2, String keyword3);
 }
--- src/main/java/fastcampus/saladbank/biz/service/LoanService.java
+++ src/main/java/fastcampus/saladbank/biz/service/LoanService.java
@@ -2,10 +2,12 @@ package fastcampus.saladbank.biz.service;
 
 import fastcampus.saladbank.biz.domain.Loan;
 import fastcampus.saladbank.biz.repository.LoanRepository;
+import fastcampus.saladbank.web.dto.LoanForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,7 +18,13 @@ public class LoanService {
     private final LoanRepository loanRepository;
 
     public List getLoanList() {
-        return loanRepository.findLoanAll();
+        List<LoanForm> loanForms = new ArrayList<>();
+        List<Loan> loanList = loanRepository.findAll();
+        for (Loan loan : loanList) {
+            LoanForm loanForm = new LoanForm(loan);
+            loanForms.add(loanForm);
+        }
+        return loanForms;
     }
 
     public Loan getLoan(long loanId) {
