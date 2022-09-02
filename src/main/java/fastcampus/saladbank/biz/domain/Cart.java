4416df54a21c2853f82ab2d100e45bca4c0beb54
wijoonwu <devjoon@nate.com>
2022-09-02 20:56:21 +0900

    Update Cart.java

--- src/main/java/fastcampus/saladbank/biz/domain/Cart.java
+++ src/main/java/fastcampus/saladbank/biz/domain/Cart.java
@@ -36,7 +36,6 @@ public class Cart extends BaseTime {
 //    private List<Card> cardList = new ArrayList<>();
 
     @OneToMany(mappedBy = "cart", cascade = ALL)
-    @Builder.Default
     private final List<CartItem> cartItems = new ArrayList<>();
 
     public void setMember(Member member){this.member = member;}
